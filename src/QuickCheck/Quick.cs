40012a48a3c72927b9d7c8dc1a09148ea28cc61a
Jacob Stanley <jacob@stanley.io>
2012-08-09 04:35:03 +0800

    Trying out simple injector instead of dictionary of type/generator pairs

--- src/QuickCheck/Quick.cs
+++ src/QuickCheck/Quick.cs
@@ -1,127 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using QuickCheck.Random;
+using QuickCheck.SimpleInjector;
+using SimpleInjector;
 
 namespace QuickCheck
 {
     public static class Quick
     {
         private static readonly IRandomFactory s_Factory;
-        private static readonly Dictionary<Type, object> s_Generators;
-        private static readonly Dictionary<Type, Type> s_GeneratorTypes;
+        private static readonly Container s_Container;
 
         static Quick()
         {
             s_Factory = new MwcFactory();
-            s_Generators = new Dictionary<Type, object>();
-            s_GeneratorTypes = new Dictionary<Type, Type>();
+            s_Container = new Container();
 
             Register(typeof(Quick).Assembly);
         }
 
         public static IGenerator<T> Generator<T>()
         {
-            object generator;
-
-            if (!s_Generators.TryGetValue(typeof(T), out generator))
-            {
-                Type generatorType = GeneratorType(typeof(T));
-                generator = Activator.CreateInstance(generatorType);
-
-                s_Generators.Add(typeof(T), generator);
-            }
-
-            return (IGenerator<T>)generator;
+            return s_Container.GetInstance<IGenerator<T>>();
         }
 
         public static void Register(Assembly assembly)
         {
-            foreach (Type generator in assembly.GetTypes())
-            {
-                Register(generator);
-            }
-        }
-
-        public static void Register(Type generator)
-        {
-            foreach (Type generatable in GeneratableTypes(generator))
-            {
-                s_GeneratorTypes.Add(
-                    GenericDef(generatable),
-                    GenericDef(generator));
-            }
-        }
-
-        public static void Register<A, V>()
-            where A : IGenerator<V>
-        {
-            Register(typeof(A));
-        }
-
-        private static Type GenericDef(Type type)
-        {
-            if (type.IsArray)
-            {
-                return typeof(Array);
-            }
-
-            if (type.IsGenericType)
-            {
-                return type.GetGenericTypeDefinition();
-            }
-
-            return type;
-        }
-
-        private static IEnumerable<Type> GeneratableTypes(Type type)
-        {
-            if (type.GetConstructors().All(x => x.GetParameters().Length != 0))
-            {
-                return new Type[0];
-            }
-
-            return type
-                .GetInterfaces()
-                .Where(IsArbitrary)
-                .Select(x => x.GetGenericArguments().First())
-                .ToArray();
-        }
-
-        private static bool IsArbitrary(Type type)
-        {
-            if (type.IsGenericType)
-            {
-                return type.GetGenericTypeDefinition() == typeof(IGenerator<>);
-            }
-
-            return false;
-        }
-
-        private static Type GeneratorType(Type value)
-        {
-            Type generic = GenericDef(value);
-
-            Type generator;
-            if (!s_GeneratorTypes.TryGetValue(generic, out generator))
-            {
-                // TODO: C# names for types
-                throw new InvalidOperationException(
-                    "Could not find a generator for " + generic.Name + ", please register one.");
-            }
-
-            if (generic == typeof(Array))
-            {
-                return generator.MakeGenericType(value.GetElementType());
-            }
-
-            if (generator.IsGenericTypeDefinition)
-            {
-                return generator.MakeGenericType(value.GetGenericArguments());
-            }
-
-            return generator;
+            s_Container.RegisterManySingles(typeof(IGenerator<>), assembly);
         }
 
         public static Result Test(ITestable test)
